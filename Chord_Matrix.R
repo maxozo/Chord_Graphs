745db4a94b20a86142c7f9460aba2014c50d3c8c
mo11 <mo11@sanger.ac.uk>
2022-12-08 11:11:47 +0000

    matrix type of input

--- Chord_Matrix.R
+++ Chord_Matrix.R
@@ -3,18 +3,17 @@
 # https://www.rdocumentation.org/packages/circlize/versions/0.4.15/topics/circos.text
 # library
 library(circlize)
-library(glue)
-layout(matrix(1:0.7, 0.7, 0.7))
+library(hash)
 
 cols_all  = rand_color(20)
 args = commandArgs(trailingOnly=TRUE)
-arg0 = args[1]
-df2 <- read.csv('/Users/mo11/work/Chord_Plots/sample_data/matrix_sample.tsv', header=TRUE,sep='\t',row.names = 1,)
-df2 <- read.csv(arg0, header=TRUE, row.names="X")
+arg1 = args[1]
+arg2 = args[2]
+df2 <- read.csv(arg1, header=TRUE,sep='\t',row.names = 1,)
 dims = dim(df2)[1]+dim(df2)[2]-2
-par(mar = c(0.5), bg=rgb(1,1,1,0.1) )
+
 factors = 1:dims
-library(hash)
+
 # Produce input vector for the rows and columns and color accordingly
 color_string = c()
 text_string = c()
@@ -57,25 +56,47 @@ for (val1 in row.names(df2))
         for (val2 in colnames(df2))
         {
             if(val2!='color'){
-                print(glue(' {val1} -- {val2}.'))
-                print(df2[c(val1),c(val2)])
                 if (!is.na( df2[c(val1),c(val2)])){
                     d = h[[val1]]
                     if(is.null(d)){
                         h[val1] <- c(val2)
                     }else{
-                    print('no')
-                    gfg <-h[[val1]]
-                    gfg <- append(gfg, val2)
-                    h[val1]<-gfg
+                        gfg <-h[[val1]]
+                        gfg <- append(gfg, val2)
+                        h[val1]<-gfg
                     }
                 }
             }
         }
     }
 }
+res <- 144
+# svglite(arg1, width = 1080/res, height = 820/res,scaling = 0.2,)
 
-circos.par(cell.padding = c(0, 0, 0, 0)) 
+# svg("/Users/mo11/work/Chord_Plots/my_plot3.svg",width = 1080/res, height = 820/res)
+# layout(matrix(1:1, 1, 1),respect = TRUE)
+# circos.par(cell.padding = c(0, 0, 0, 0),circle.margin=c(0.9,0.9,0.9,0.9)) 
+# circos.initialize(factors, xlim = c(0, .5)) 
+# count=0
+# circos.trackPlotRegion(ylim = c(0, 1), track.height = 0.1, bg.col = color_string, bg.border = NA, panel.fun = function(x, y) {
+#     l = CELL_META$sector.numeric.index
+#     circos.text(CELL_META$xcenter[1]- mm_x(6), CELL_META$cell.ylim[2] + mm_y(2),
+#         text_string[l], facing = "reverse.clockwise", niceFacing = TRUE,
+#         adj = c(1, 0.5), cex = 0.4,col = color_string[l])
+    
+#     for(v1 in h[[text_string[l]]]){
+#         color = link_colors[[as.character(df2[text_string[l],v1])]]
+#         circos.link(l, 0.25, indexes[[v1]], 0.25, col = color,lwd = 2,)
+#     }
+    
+# } )
+
+# dev.off()
+
+pdf(arg2,width = 1080/res, height = 820/res)
+layout(matrix(1:1, 1, 1),respect = TRUE)
+# par(mar = c(1), bg=rgb(1,1,1,1) )
+circos.par(cell.padding = c(0, 0, 0, 0),circle.margin=c(0.9,0.9,0.9,0.9)) 
 circos.initialize(factors, xlim = c(0, .5)) 
 count=0
 circos.trackPlotRegion(ylim = c(0, 1), track.height = 0.1, bg.col = color_string, bg.border = NA, panel.fun = function(x, y) {
@@ -90,11 +111,4 @@ circos.trackPlotRegion(ylim = c(0, 1), track.height = 0.1, bg.col = color_string
     }
     
 } )
-
-
-# add links
-for(i in 1:2) {
-    se = sample(factors, 2)
-    circos.link(se[1], c(1,1), se[2], runif(2), col = rand_color(1, transparency = 0.4)) 
-    
-}
+dev.off()
--- README.md
+++ README.md
@@ -13,6 +13,9 @@ install_github('wencke/wencke.github.io')
 
 
 After instalation:
+
+For the expression type of plots as in - 
+
 prepeare 2 files - 
 1) Quantifiable file (like in example file ./sample_data/sample_data1.tsv)  - first col must be Identifier, second column can be any quantifiable value of your interest, does not have to be labeled logFC: 
 Identifier	logFC
@@ -29,3 +32,12 @@ G2 Phase	PAWR;IKBKG;GSN;APP;STAT1
 
 Once you have both of these files you can run:
 python /path/to/your/colned/folder/Produce_Chrod.py -o test_output_Nath.svg -f1 /path/to/your/sample_data_1.tsv -f2 /path/to/your/sample_data_2_belongings.tsv
+
+
+For matrix pathway plots type of plots as in - 
+
+Produce a pathway matrix file where first column is first identifiers and firt row is second identifiers that you want to link.
+The second column has to be color - this is a bolean number that will make coresponding rows/columns in a different color ion a circle plot(currently arbitary).
+The second row has to be color again for the same reason.
+
+The matrix represents different links. Different values will be colored differently, and empty cells will not establish link between two coresponding cells.
